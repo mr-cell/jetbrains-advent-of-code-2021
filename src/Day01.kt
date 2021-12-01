482bf90ded6548304ccf5b27b2cfaa5ec1da1161
mr-cell <marcel.schally@gmail.com>
2021-12-01 20:00:23 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,17 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun countDepthIncreases(input: List<String>): Int {
+        return input.map { it.toInt() }.zipWithNext().count { it.first < it.second }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }.windowed(3).map { it.sum() }.zipWithNext().count { it.first < it.second }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(countDepthIncreases(testInput) == 7)
 
     val input = readInput("Day01")
-    println(part1(input))
+    println(countDepthIncreases(input))
     println(part2(input))
 }
