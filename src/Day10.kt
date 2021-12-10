e9c4010aad56d5361a178f3ff5c6afbd9b57eb60
mr-cell <marcel.schally@gmail.com>
2021-12-10 20:26:55 +0100

    day 10 (better)

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,113 +1,65 @@
 import java.util.*
-import kotlin.math.floor
+
+class Day10(private val input: List<String>) {
+    companion object {
+        private val scoresPart1 = mapOf(')' to 3, ']' to 57, '}' to 1197, '>' to 25137)
+        private val scoresPart2 = mapOf(')' to 1, ']' to 2, '}' to 3, '>' to 4)
+        private val openToClosed = mapOf('(' to ')', '[' to ']', '{' to '}', '<' to '>')
+    }
+
+    fun solvePart1(): Long {
+        return input
+            .map { parseRow(it) }
+            .filterIsInstance<Corrupted>()
+            .sumOf { it.score().toLong() }
+    }
+
+    fun solvePart2(): Long {
+        return input
+            .map { parseRow(it) }
+            .filterIsInstance<Incomplete>()
+            .map { it.score() }
+            .sorted()
+            .midpoint()
+    }
+
+    private fun parseRow(row: String): ParseResult {
+        val stack = ArrayDeque<Char>()
+        row.forEach { symbol ->
+            when {
+                symbol in openToClosed -> stack.addLast(symbol)
+                symbol != openToClosed[stack.removeLast()] -> return Corrupted(symbol)
+            }
+        }
+
+        return if (stack.isEmpty()) Success else Incomplete(stack.reversed())
+    }
+
+    private sealed interface ParseResult
+    private object Success: ParseResult
+    private class Incomplete(val pending: List<Char>): ParseResult {
+        fun score(): Long =
+            pending
+                .map { openToClosed.getValue(it) }
+                .fold(0) { acc, symbol -> (acc * 5) + scoresPart2.getValue(symbol) }
+    }
+    private class Corrupted(val actual: Char): ParseResult {
+        fun score(): Int = scoresPart1.getValue(actual)
+    }
+}
+
+fun <T> List<T>.midpoint(): T = this[lastIndex/2]
 
 fun main() {
 
-    fun part1(inputData: List<String>): Long {
-        var counter = 0L
-        for (entry in inputData) {
-            val entryChars = entry.toCharArray()
-            val stack = Stack<Char>()
-            for (char in entryChars) {
-                when (char) {
-                    '(' -> stack.add(')')
-                    '[' -> stack.add(']')
-                    '{' -> stack.add('}')
-                    '<' -> stack.add('>')
-                    ')' -> {
-                        val last = stack.pop()
-                        if (last != char) {
-                            counter += 3
-                            break
-                        }
-                    }
-                    ']' -> {
-                        val last = stack.pop()
-                        if (last != char) {
-                            counter += 57
-                            break
-                        }
-
-                    }
-                    '}' -> {
-                        val last = stack.pop()
-                        if (last != char) {
-                            counter += 1197
-                            break
-                        }
-                    }
-                    '>' -> {
-                        val last = stack.pop()
-                        if (last != char) {
-                            counter += 25137
-                            break
-                        }
-                    }
-                }
-            }
-        }
-        return counter
-    }
-
-    fun part2(inputData: List<String>): Long {
-        val scores = mutableListOf<Long>()
-        outer@ for (entry in inputData) {
-            val entryChars = entry.toCharArray()
-            val stack = Stack<Char>()
-            for (char in entryChars) {
-                when (char) {
-                    '(' -> stack.add(')')
-                    '[' -> stack.add(']')
-                    '{' -> stack.add('}')
-                    '<' -> stack.add('>')
-                    ')' -> {
-                        val last = stack.pop()
-                        if (last != char) {
-                            continue@outer
-                        }
-
-                    }
-                    ']' -> {
-                        val last = stack.pop()
-                        if (last != char) {
-                            continue@outer
-                        }
-                    }
-                    '}' -> {
-                        val last = stack.pop()
-                        if (last != char) {
-                            continue@outer
-                        }
-                    }
-                    '>' -> {
-                        val last = stack.pop()
-                        if (last != char) {
-                            continue@outer
-                        }
-                    }
-                }
-            }
-            var score = 0L
-            while (stack.isNotEmpty()) {
-                when (stack.pop()) {
-                    ')' -> score = (score * 5) + 1
-                    ']' -> score = (score * 5) + 2
-                    '}' -> score = (score * 5) + 3
-                    '>' -> score = (score * 5) + 4
-                }
-            }
-            scores.add(score)
-        }
-        scores.sort()
-        return scores[floor((scores.size / 2).toDouble()).toInt()]
-    }
-
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day10_test")
-    check(part1(testInput) == 26397L)
-    check(part2(testInput) == 288957L)
+    val day10Test = Day10(testInput)
+    check(day10Test.solvePart1() == 26397L)
+    check(day10Test.solvePart2() == 288957L)
 
     val input = readInput("Day10")
-    println(part1(input))
-    println(part2(input))
+    val day10 = Day10(input)
+    println(day10.solvePart1())
+    println(day10.solvePart2())
 }
